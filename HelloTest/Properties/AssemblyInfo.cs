27dec15ec80a740fe1ff074b793dc65d44913321
Alex Gaultier <alexandre.gaultier@live.fr>
2020-10-04 00:00:42 +0200

    Ajoutez des fichiers projet.

---
+++ HelloTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf9007a5-a0ac-4810-b911-ad06d27e8094")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MaDeuxièmeApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MaDeuxièmeApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(createMessage());
+            Console.WriteLine("Bonjour");
+        }
+        
+        public static string createMessage()
+        {
+            return "Hello world";
+            
+        }
+    }
+}
